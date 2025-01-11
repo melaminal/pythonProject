d6e7cdec0d2b150976f8ffd4a806bd60f3fadf1b
melaminal <mikhailovamariiaalexandrovna@gmail.com>
2025-01-11 14:48:18 +0200

    11.01.2025

--- PhD madness/rotations_directly.py
+++ PhD madness/rotations_directly.py
@@ -152,6 +152,28 @@ def update_atom_positions(structure, glazer_type):
             else:
                 new_sites.append(site)
 
+    # Вращения для a-a-a-
+    elif glazer_type == "a-a-a-":
+        predefined_coords = {
+            9: [0.65000, 0.35000, 0.35000],
+            10: [0.85000, 0.15000, 0.65000],
+            11: [0.35000, 0.65000, 0.15000],
+            12: [0.15000, 0.85000, 0.85000],
+            13: [0.15000, 0.15000, 0.15000],
+            14: [0.35000, 0.35000, 0.85000],
+            15: [0.85000, 0.85000, 0.35000],
+            16: [0.65000, 0.65000, 0.65000],
+            17: [-0.20000, 0.50000, 0.00000],
+            18: [0.20000, 0.50000, 0.50000],
+            19: [0.70000, 0.00000, 0.00000],
+            20: [0.30000, 0.00000, 0.50000]
+        }
+        for i, site in enumerate(structure.sites):
+            if i + 1 in predefined_coords:
+                new_coords = predefined_coords[i + 1]
+                new_sites.append(PeriodicSite(site.species, new_coords, structure.lattice))
+            else:
+                new_sites.append(site)
 
     else:  # Если вращений нет
         new_sites = structure.sites
@@ -159,7 +181,7 @@ def update_atom_positions(structure, glazer_type):
     return Structure.from_sites(new_sites)
 
 # Пример обработки структуры с вращениями
-glazer_type = "a+b-b-"  # Замените на нужный тип-
+glazer_type = "a-a-a-"  # Замените на нужный тип
 updated_structure = update_atom_positions(EuTiO3_structure, glazer_type)
 
 # Анализируем пространственную группу
--- PhD madness/symmetry_det.py
+++ PhD madness/symmetry_det.py
@@ -1,7 +1,8 @@
 from pymatgen.io.vasp import Poscar
 from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
 
-poscar = Poscar.from_file("/home/dieguez/Desktop/POSCAR")
+# poscar = Poscar.from_file("/home/dieguez/Desktop/EuTiO3/Rotations/a0a0a0/POSCAR")
+poscar = Poscar.from_file("C:/Users/Maria/Desktop/POSCAR")
 structure = poscar.structure
 
 sga = SpacegroupAnalyzer(structure, symprec=0.0000001, angle_tolerance=1)
