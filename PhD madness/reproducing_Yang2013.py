bbe50ee23fa72855f94b7ca635269491a2394029
melaminal <mikhailovamariiaalexandrovna@gmail.com>
2025-03-06 19:16:23 +0200

    06.03.2025

--- PhD madness/reproducing_Yang2013.py
+++ PhD madness/reproducing_Yang2013.py
@@ -1,21 +1,16 @@
 import matplotlib.pyplot as plt
 
 # Данные
-x_P4_mmm = [-5.00, -4.00, -3.00, -2.00, -1.00, 0.00, 1.00, 2.00, 3.00, 4.00, 5.00] # Фаза проверена
-y_P4_mmm = [-199.154365510 / 4, -199.828844700 / 4, -200.325823990 / 4, -200.662947640 / 4, -200.855441340 / 4,
-            -200.918217210 / 4, -200.863757400 / 4, -200.702980610 / 4, -200.445338970 / 4, -200.099874900 / 4,
-            -199.674896790 / 4]
-
-x_I4mcm = [-4.00, -3.00, -2.00, -1.00, 0.00, 0.50, 1.00, 2.00] # Фаза проверена
-y_I4mcm = [-200.462847030 / 4, -200.765920710 / 4, -200.959607610 / 4, -201.049075670 / 4, -201.038515920 / 4,
+x_I4_mcm = [-4.00, -3.00, -2.00, -1.00, 0.00, 0.50, 1.00, 2.00] # Фаза проверена
+y_I4_mcm = [-200.462847030 / 4, -200.765920710 / 4, -200.959607610 / 4, -201.049075670 / 4, -201.038515920 / 4,
            -200.999170270 / 4, -200.935652040 / 4, -200.742049550 / 4]
 
 x_P4mm = [-8.00, -7.00, -6.00, -5.00, -4.00, -3.00] # Фаза проверена
 y_P4mm = [-198.943953460 / 4, -199.285493010 / 4, -199.564980710 / 4, -199.808363760 / 4, -200.079119550 / 4,
           -200.403009200 / 4]
 
-x_C2_m = [-1.00, 0.00, 1.00, 2.00, 3.00] # Фаза проверена
-y_C2_m = [-201.049080990 / 4, -201.050489990 / 4, -200.992754040 / 4, -200.835519510 / 4, -200.579417690 / 4]
+# x_Imma = [-2.00, -1.00, 0.00, 1.00, 2.00, 3.00]
+# y_Imma = []
 
 x_Pnma = [-1.00, 0.00, 1.00, 2.00, 3.00] # Фаза проверена
 y_Pnma = [-200.997767250 / 4, -201.053085670 / 4, -200.999898660 / 4, -200.840539020 / 4, -200.582906320 / 4]
@@ -27,30 +22,31 @@ x_Pmc2_1 = [2.00, 3.00, 4.00, 5.00, 6.00, 7.00] # Фаза проверена
 y_Pmc2_1 = [-200.863851580 / 4, -200.691550890 / 4, -200.487917020 / 4, -200.258320800 / 4, -200.007569500 / 4,
             -199.740011790 / 4]
 
-x_I4cm = [-7.00, -6.00, -5.00, -4.00, -3.00, -2.00]
-y_I4cm = []
+x_I4cm = [-7.00, -6.00, -5.00, -4.00, -3.00, -2.00] # Фаза проверена
+y_I4cm = [-199.058979720 / 4, -199.583121550 / 4, -200.059696990 / 4, -200.462840000 / 4, -200.765896130 / 4,
+          -200.959610370 / 4]
 
-x_Ima2 = [0.00, 1.00, 2.00, 3.00, 4.00, 5.00, 6.00, 7.00]
-y_Ima2 = [-201.038565010 / 4, -200.937097030 / 4, -200.784318220 / 4, -200.600821300 / 4, -200.390629910 / 4,
-          -200.158382640 / 4, -199.909399670 / 4, -199.649103970 / 4]
+x_Ima2 = [0.00, 1.00, 2.00, 3.00, 4.00, 5.00, 6.00, 7.00] # Фаза проверена
+y_Ima2 = [-201.039256210 / 4, -200.936202140 / 4, -200.780555550 / 4, -200.594596760 / 4, -200.381842180 / 4,
+          -200.146985260 / 4, -199.895204080 / 4, -199.632142410 / 4]
 
 # Построение графиков
 plt.figure(figsize=(7, 6))
-
-plt.plot(x_P4_mmm, y_P4_mmm, marker='o', label="P4/mmm")
-plt.plot(x_I4mcm, y_I4mcm, marker='o', label="I4/mcm (a0a0c-)")
-plt.plot(x_P4mm, y_P4mm, marker='o', label="P4mm")
-plt.plot(x_Pnma, y_Pnma, marker='o', label="Pnma")
-plt.plot(x_Pmc2_1, y_Pmc2_1, marker='o', label="Pmc2_1")
-plt.plot(x_I4cm , y_I4cm, marker='o', label="I4cm")
-plt.plot(x_Ima2, y_Ima2, marker='o', label="Ima2")
+plt.plot(x_I4_mcm, y_I4_mcm, color='k', marker='s', markerfacecolor='k',  markeredgecolor='k', markersize=6, label="I4/mcm")
+plt.plot(x_P4mm, y_P4mm, color='#800080', markerfacecolor='#800080',  markeredgecolor='#800080', marker='o', label="P4mm")
+# plt.plot(x_Imma, y_Imma, marker='D', color='lightseagreen', markerfacecolor='lightseagreen',  markeredgecolor='lightseagreen', label="Imma")
+plt.plot(x_Pnma, y_Pnma, color='#A52A2A', markerfacecolor='#A52A2A',  markeredgecolor='#A52A2A', marker='>', label="Pnma")
+plt.plot(x_C2_c, y_C2_c, marker='^', color='chartreuse', markerfacecolor='chartreuse',  markeredgecolor='chartreuse', label="C2/c")
+plt.plot(x_Pmc2_1, y_Pmc2_1, marker='H', color='olive', markerfacecolor='olive',  markeredgecolor='olive', label="Pmc2_1")
+plt.plot(x_I4cm , y_I4cm, color='m', markerfacecolor='m',  markeredgecolor='m', marker='<', label="I4cm")
+plt.plot(x_Ima2, y_Ima2, marker='v', color='b', markerfacecolor='b',  markeredgecolor='b', label="Ima2")
 # Настройка графика
-plt.title("Biaxial")
-plt.xlabel("Biaxial strain (%)", fontsize=14)
+plt.title("Biaxial strain")
+plt.xlabel("Misfit strain (%)", fontsize=14)
 plt.ylabel("Energy (eV)", fontsize=14)
 plt.tick_params(axis='both', direction='in', labelsize=12)
 plt.legend()
-plt.grid(True)
 
-# Показ графика
-plt.show()
+# Сохранение графика
+# plt.show()
+plt.savefig("my_plot.png")
--- PhD madness/uniaxial_strain.py
+++ PhD madness/uniaxial_strain.py
@@ -1,40 +1,52 @@
 import matplotlib.pyplot as plt
 
 # Данные
-x_Pm3m = range(0, 11)
-y_Pm3m = [-200.918270340 / 4, -200.898029310 / 4, -200.837320370 / 4, -200.739733950 / 4, -200.609502880 / 4, -200.450159380 / 4,
-        -200.264782980 / 4, -200.056426620 / 4, -199.827642980 / 4, -199.580334690 / 4, -199.317499170 / 4]
+x_I4_mcm = [-4.00, -3.00, -2.00, -1.00, 0.00, 0.50, 1.00, 2.00] # Фаза проверена
+y_I4_mcm = [-200.462847030 / 4, -200.765920710 / 4, -200.959607610 / 4, -201.049075670 / 4, -201.038515920 / 4,
+           -200.999170270 / 4, -200.935652040 / 4, -200.742049550 / 4]
 
-x_I4mcm = [0.00, 3.00, 6.00, 7.00, 8.00, 9.00, 10.00]
-y_I4mcm = [-201.049690720 / 4, -201.023564620 / 4, -200.575824320 / 4, -200.398323900 / 4, -200.199599690 / 4, -199.981883460 / 4,
-           -199.770304080 / 4]
+x_P4mm = [-8.00, -7.00, -6.00, -5.00, -4.00, -3.00] # Фаза проверена
+y_P4mm = [-198.943953460 / 4, -199.285493010 / 4, -199.564980710 / 4, -199.808363760 / 4, -200.079119550 / 4,
+          -200.403009200 / 4]
 
-x_P4mbm = [0.00, 1.00, 2.00, 3.00, 8.00, 9.00, 10.00]
-y_P4mbm = [-200.935420210 / 4, -200.929710560 / 4, -200.895335580 / 4, -200.817204630 / 4, -200.064352120 / 4, -199.851633530 / 4,
-           -199.644605040 / 4]
+# x_Imma = [-2.00, -1.00, 0.00, 1.00, 2.00, 3.00]
+# y_Imma = []
 
-x_Imma = [2.00, 5.00, 6.00, 8.00]
-y_Imma = [-201.023555270 / 4, -200.729709800 / 4, -200.575805510 / 4, -200.199598930 / 4]
+x_Pnma = [-1.00, 0.00, 1.00, 2.00, 3.00] # Фаза проверена
+y_Pnma = [-200.997767250 / 4, -201.053085670 / 4, -200.999898660 / 4, -200.840539020 / 4, -200.582906320 / 4]
 
-x_C2m = [0.00, 1.00, 2.00, 3.00, 10.00]
-y_C2m = [-201.057920040 / 4, -201.054818510 / 4, -201.023559140 / 4, -200.956618150 / 4, -199.770302340  / 4]
+x_C2_c = [-1.00, 0.00, 1.00, 2.00, 3.00] # Фаза проверена
+y_C2_c = [-201.049084340 / 4, -201.052852100 / 4, -200.999895620 / 4, -200.840498300 / 4, -200.582944320 / 4]
+
+x_Pmc2_1 = [2.00, 3.00, 4.00, 5.00, 6.00, 7.00] # Фаза проверена
+y_Pmc2_1 = [-200.863851580 / 4, -200.691550890 / 4, -200.487917020 / 4, -200.258320800 / 4, -200.007569500 / 4,
+            -199.740011790 / 4]
+
+x_I4cm = [-7.00, -6.00, -5.00, -4.00, -3.00, -2.00] # Фаза проверена
+y_I4cm = [-199.058979720 / 4, -199.583121550 / 4, -200.059696990 / 4, -200.462840000 / 4, -200.765896130 / 4,
+          -200.959610370 / 4]
+
+x_Ima2 = [0.00, 1.00, 2.00, 3.00, 4.00, 5.00, 6.00, 7.00] # Фаза проверена
+y_Ima2 = [-201.039256210 / 4, -200.936202140 / 4, -200.780555550 / 4, -200.594596760 / 4, -200.381842180 / 4,
+          -200.146985260 / 4, -199.895204080 / 4, -199.632142410 / 4]
 
 # Построение графиков
 plt.figure(figsize=(7, 6))
-
-plt.plot(x_Pm3m, y_Pm3m, marker='o', label="P4/mmm")
-plt.plot(x_I4mcm, y_I4mcm, marker='o', label="I4/mcm (a0a0c-)")
-plt.plot(x_P4mbm, y_P4mbm, marker='o', label="P4/mbm (a0a0c+)")
-plt.plot(x_Imma, y_Imma, marker='o', label="Imma -> C2/m")
-plt.plot(x_C2m, y_C2m, marker='o', label="C2m (a0b-c-)")
-
+plt.plot(x_I4_mcm, y_I4_mcm, color='k', marker='s', markerfacecolor='k',  markeredgecolor='k', markersize=6, label="I4/mcm")
+plt.plot(x_P4mm, y_P4mm, color='#800080', markerfacecolor='#800080',  markeredgecolor='#800080', marker='o', label="P4mm")
+# plt.plot(x_Imma, y_Imma, marker='D', color='lightseagreen', markerfacecolor='lightseagreen',  markeredgecolor='lightseagreen', label="Imma")
+plt.plot(x_Pnma, y_Pnma, color='#A52A2A', markerfacecolor='#A52A2A',  markeredgecolor='#A52A2A', marker='>', label="Pnma")
+plt.plot(x_C2_c, y_C2_c, marker='^', color='chartreuse', markerfacecolor='chartreuse',  markeredgecolor='chartreuse', label="C2/c")
+plt.plot(x_Pmc2_1, y_Pmc2_1, marker='H', color='olive', markerfacecolor='olive',  markeredgecolor='olive', label="Pmc2_1")
+plt.plot(x_I4cm , y_I4cm, color='m', markerfacecolor='m',  markeredgecolor='m', marker='<', label="I4cm")
+plt.plot(x_Ima2, y_Ima2, marker='v', color='b', markerfacecolor='b',  markeredgecolor='b', label="Ima2")
 # Настройка графика
-plt.title("Uniaxial")
-plt.xlabel("Uniaxial strain (%)", fontsize=14)
+plt.title("Biaxial strain")
+plt.xlabel("Misfit strain (%)", fontsize=14)
 plt.ylabel("Energy (eV)", fontsize=14)
 plt.tick_params(axis='both', direction='in', labelsize=12)
 plt.legend()
-plt.grid(True)
 
-# Показ графика
-plt.show()
+# Сохранение графика
+# plt.show()
+plt.savefig("my_plot.png")
